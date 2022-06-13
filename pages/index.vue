9d866afd214d80b1a35eae1c8a411d6d13ba8f53
Warsono <warsono16694@gmail.com>
2022-06-13 16:59:19 +0700

    feat: dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,22 @@ useHead({
 
 <template>
   <div>
-    <v-card title="Welcome"> Welcome to your new GITS Project! </v-card>
+    <PageHeader title="Dashboard" />
+
+    <div class="grid grid-cols-2 sm:grid-cols-4 gap-6">
+      <v-stats title="Users" value="20" icon="ri:user-line" />
+      <v-stats title="Posts" value="20" icon="ri:table-line" color="success" />
+      <v-stats
+        title="Comments"
+        value="20"
+        icon="ri:message-line"
+        color="warning"
+      />
+      <v-stats title="Pages" value="5" icon="ri:book-line" color="error" />
+    </div>
+
+    <v-card title="Welcome" hide-footer class="mt-6">
+      Welcome to your new GITS Project!
+    </v-card>
   </div>
 </template>
