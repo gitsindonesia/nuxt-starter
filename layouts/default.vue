e9ce3a2b0d2fcaadfde17adc686896cc9ebe3349
Warsono <warsono16694@gmail.com>
2022-06-14 17:10:20 +0700

    chore: improve responsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { isMini } from '~~/composables/layout';
+import { Icon } from '@iconify/vue';
 
 useHead({
   title: 'Nuxt 3 + GITS UI',
@@ -15,9 +16,18 @@ useHead({
       :class="isMini ? 'sm:ml-24' : 'sm:ml-[270px]'"
     >
       <v-app-bar
-        class="px-4 py-2 shadow sm:hidden sticky top-0 w-full inset-x-0"
-        @toggleMenu="isMini = !isMini"
-      />
+        class="px-2 py-2 shadow sm:hidden sticky top-0 w-full inset-x-0"
+      >
+        <div class="flex">
+          <v-btn @click="isMini = !isMini" icon rounded text>
+            <Icon icon="ri:menu-line" class="w-6 h-6" />
+          </v-btn>
+          <div class="flex-1">
+            <v-logo img-class="h-8 my-1 mx-auto" />
+          </div>
+          <div class="w-6"></div>
+        </div>
+      </v-app-bar>
 
       <div class="px-4 py-6">
         <slot />
