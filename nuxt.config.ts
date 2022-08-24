d6d746a71f557a3b7740cca3404a0f02d465bf0e
Warsono <warsono16694@gmail.com>
2022-08-24 08:08:01 +0700

    feat: add gtag plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,9 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     '@gits-id/ui-nuxt',
   ],
+  runtimeConfig: {
+    public: {
+      gaId: '',
+    },
+  },
 });
---
+++ plugins/gtag.ts
@@ -0,0 +1,13 @@
+import VueGtag from 'vue-gtag';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+
+  nuxtApp.vueApp.use(
+    VueGtag,
+    {
+      config: { id: config.public.gaId },
+    },
+    nuxtApp.$router
+  );
+});
