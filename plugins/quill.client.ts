06d8a78b126237dc10bd398f4711292ae448535e
Warsono <warsono16694@gmail.com>
2022-08-24 07:55:02 +0700

    fix: load quill css

--- plugins/quill.client.ts
+++ plugins/quill.client.ts
@@ -1,4 +1,5 @@
 import { VQuillEditor } from '@gits-id/quill-editor';
+import '@gits-id/quill-editor/dist/style.css';
 
 export default defineNuxtPlugin((nuxtapp) => {
   nuxtapp.vueApp.component('VQuillEditor', VQuillEditor);
