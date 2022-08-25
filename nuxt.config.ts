0e9ccc3fa555558d125a87a48d60c59682bc3ecd
Warsono <warsono16694@gmail.com>
2022-08-25 10:15:22 +0700

    chore: transpile deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
 import { defineNuxtConfig } from 'nuxt';
 
+const transpile = [
+  '@headlessui/vue',
+  '@heroicons/vue',
+  '@gits-id/ui',
+  '@gits-id/alert',
+  '@gits-id/app-bar',
+  '@gits-id/badge',
+  '@gits-id/breadcrumbs',
+  '@gits-id/button',
+  '@gits-id/card',
+  '@gits-id/collapsible',
+  '@gits-id/container',
+  '@gits-id/dropdown',
+  '@gits-id/editor',
+  '@gits-id/forms',
+  '@gits-id/icon',
+  '@gits-id/layouts',
+  '@gits-id/list',
+  '@gits-id/logo',
+  '@gits-id/menu',
+  '@gits-id/menus',
+  '@gits-id/modal',
+  '@gits-id/multi-select',
+  '@gits-id/nav-drawer',
+  '@gits-id/navbar',
+  '@gits-id/pages',
+  '@gits-id/pagination',
+  '@gits-id/progress-bar',
+  '@gits-id/select',
+  '@gits-id/services',
+  '@gits-id/spinner',
+  '@gits-id/stats',
+  '@gits-id/switch',
+  '@gits-id/table',
+  '@gits-id/tabs',
+  '@gits-id/textarea',
+  '@gits-id/theme',
+  '@gits-id/toast',
+  '@gits-id/tooltip',
+  '@gits-id/utils',
+];
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: [
@@ -13,7 +55,12 @@ export default defineNuxtConfig({
       gaId: '',
     },
   },
+  vite: {
+    optimizeDeps: {
+      include: transpile,
+    },
+  },
   build: {
-    transpile: ['@gits-id/dropdown'],
+    transpile,
   },
 });
