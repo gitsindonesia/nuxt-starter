14f74426fdb7567189106c32fb4eab86d6a89cef
Warsono <warsono16694@gmail.com>
2022-09-12 20:15:15 +0700

    chore: add triange accent

--- components/Home/Cta.vue
+++ components/Home/Cta.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-50">
+  <div class="bg-white">
     <div
       class="
         mx-auto
--- components/Home/Features.vue
+++ components/Home/Features.vue
@@ -51,9 +51,20 @@ const features = ref([
 ]);
 </script>
 <template>
-  <section
-    class="bg-gradient-to-tr from-indigo-600 to-indigo-800 text-white py-20"
-  >
+  <div
+    class="
+      w-0
+      h-0
+      border-t-0
+      border-t-transparent
+      border-b-[50px]
+      border-b-indigo-600
+      border-l-[100vw]
+      border-l-white
+      border-r-transparent
+    "
+  ></div>
+  <section class="bg-indigo-600 text-white py-20">
     <div class="container mx-auto">
       <div class="space-y-2">
         <h3 class="text-4xl font-semibold">Fully Featured UI Component</h3>
@@ -77,4 +88,16 @@ const features = ref([
       </div>
     </div>
   </section>
+  <div
+    class="
+      w-0
+      h-0
+      border-t-transparent
+      border-t-[50px]
+      border-t-indigo-600
+      border-r-[100vw]
+      border-l-transparent
+      border-r-white
+    "
+  ></div>
 </template>
