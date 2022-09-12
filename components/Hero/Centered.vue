3a94a8b002149c62c35816d7f3a1e951eed1d129
Warsono <warsono16694@gmail.com>
2022-09-12 19:48:52 +0700

    chore(Hero Centered): update layout

--- components/Hero/Centered.vue
+++ components/Hero/Centered.vue
@@ -6,17 +6,17 @@ defineProps<{
 </script>
 
 <template>
-  <section class="bg-white">
+  <section class="bg-white h-[calc(100vh-70px)] grid place-items-center">
     <div class="container mx-auto text-center py-20">
       <slot name="title" :title="title">
         <h1
           class="
-            text-8xl
+            text-7xl
             font-black
             text-transparent
             bg-clip-text bg-gradient-to-r
             from-primary-500
-            to-green-500
+            to-indigo-600
           "
         >
           {{ title }}
