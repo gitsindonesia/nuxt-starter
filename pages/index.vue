d2ce7680d458a6675e0cdf0bee26c066ef2c5c0f
Warsono <warsono16694@gmail.com>
2022-09-12 18:58:08 +0700

    refactor: extract section into SFC

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,14 @@
 definePageMeta({
   layout: 'landing',
 });
+
+useHead({
+  title: 'Home'
+})
 </script>
 
 <template>
-  <div class="container mx-auto px-6 py-5 border-4 rounded-xl border-dashed">
-    <h1 class="font-semibold text-3xl">Welcome to your new project!</h1>
-    <p class="mt-4">
-      Look at the
-      <a class="font-medium underline text-primary" href="https://gits-ui.web.app"> GITS UI documentation </a> and
-      <a class="font-medium underline text-primary" href="https://v3.nuxtjs.org">nuxt 3 documentation </a> to learn more.
-    </p>
+  <div class="container mx-auto">
+   <HomeWelcome />
   </div>
 </template>
