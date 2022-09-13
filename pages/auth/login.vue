ec7df1f3202a7b831256074430d48d93f00378cb
Warsono <warsono16694@gmail.com>
2022-09-13 15:17:01 +0700

    chore: add root element to handle transition error

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -12,50 +12,56 @@ const { handleSubmit, showAlert, showPassword, error, loading } = useLogin();
 </script>
 
 <template>
-  <AuthCard>
-    <v-logo img-class="h-10 mx-auto" />
+  <div>
+    <AuthCard>
+      <v-logo img-class="h-10 mx-auto" />
 
-    <h1 class="text-center text-3xl font-semibold mt-4 text-gray-800">
-      Welcome back!
-    </h1>
-    <p class="text-gray-500 text-center mt-2">Log in to your account</p>
+      <h1 class="text-center text-3xl font-semibold mt-4 text-gray-800">
+        Welcome back!
+      </h1>
+      <p class="text-gray-500 text-center mt-2">Log in to your account</p>
 
-    <v-alert v-model="showAlert" color="error" class="mt-6">
-      {{ error }}
-    </v-alert>
+      <v-alert v-model="showAlert" color="error" class="mt-6">
+        {{ error }}
+      </v-alert>
 
-    <form class="mt-4" @submit="handleSubmit">
-      <v-input
-        label="Email"
-        placeholder="Email"
-        name="email"
-        prepend-icon="ri:mail-line"
-      />
-      <v-input
-        wrapper-class="mt-4"
-        label="Password"
-        placeholder="Password"
-        name="password"
-        :type="showPassword ? 'text' : 'password'"
-        prepend-icon="ri:lock-line"
-        append-icon="ri:eye-line"
-        @clickPrependIcon="showPassword = !showPassword"
-      />
-      <v-checkbox wrapper-class="mt-4" name="rememberMe" label="Remember me" />
-      <v-btn
-        :loading="loading"
-        type="submit"
-        class="mt-4"
-        color="primary"
-        block
-      >
-        Login
+      <form class="mt-4" @submit="handleSubmit">
+        <v-input
+          label="Email"
+          placeholder="Email"
+          name="email"
+          prepend-icon="ri:mail-line"
+        />
+        <v-input
+          wrapper-class="mt-4"
+          label="Password"
+          placeholder="Password"
+          name="password"
+          :type="showPassword ? 'text' : 'password'"
+          prepend-icon="ri:lock-line"
+          append-icon="ri:eye-line"
+          @clickPrependIcon="showPassword = !showPassword"
+        />
+        <v-checkbox
+          wrapper-class="mt-4"
+          name="rememberMe"
+          label="Remember me"
+        />
+        <v-btn
+          :loading="loading"
+          type="submit"
+          class="mt-4"
+          color="primary"
+          block
+        >
+          Login
+        </v-btn>
+      </form>
+    </AuthCard>
+    <div class="text-center mt-2">
+      <v-btn color="primary" text to="/auth/forgot-password">
+        Forgot Password?
       </v-btn>
-    </form>
-  </AuthCard>
-  <div class="text-center mt-2">
-    <v-btn color="primary" text to="/auth/forgot-password">
-      Forgot Password?
-    </v-btn>
+    </div>
   </div>
 </template>
