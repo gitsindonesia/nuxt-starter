692c766c6210eed9e184b7c2451b90807481f7a0
Warsono <warsono16694@gmail.com>
2022-11-03 11:13:40 +0700

    chore: upgrade to RC 12

--- app.vue
+++ app.vue
@@ -1,15 +1,4 @@
 <template>
-  <Head>
-    <!-- Google tag (gtag.js) -->
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-44SPLJSV5C" />
-    <Script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-44SPLJSV5C');
-    </Script>
-  </Head>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
