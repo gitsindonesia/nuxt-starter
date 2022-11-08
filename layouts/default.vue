85690de981e817645a30dd035d9be0d762f29394
Warsono <warsono16694@gmail.com>
2022-11-08 17:37:25 +0700

    chore: fix dropdown right

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import '@gits-id/avatar/dist/style.css'
+import { VBtn, VDropdownButton } from '@gits-id/ui'
 
 const layout = useLayoutStore()
 const { miniSidebar, sidebar } = storeToRefs(layout)
@@ -30,25 +31,26 @@ const toggleMenu = () => {
       class="sm:flex-1 transition-all duration-300"
     >
       <VAppBar shadow class="flex px-4 py-2 items-center justify-between">
-        <v-btn icon rounded text @click="toggleMenu">
+        <VBtn icon rounded text @click="toggleMenu">
           <v-icon name="ri:menu-line" class="w-6 h-6" />
-        </v-btn>
+        </VBtn>
         <div class="flex gap-2 items-center">
-          <v-btn icon rounded text class="relative">
+          <VBtn icon rounded text class="relative">
             <v-icon name="ri:notification-line" class="w-6 h-6" />
             <span class="bg-error-500 text-white rounded-full absolute right-0 top-0 w-4 h-4 text-xs">
               6
             </span>
-          </v-btn>
+          </VBtn>
           <v-dropdown
             :items="[
               {
                 text: 'Logout',
               },
             ]"
+            right
           >
-            <template #activator="on">
-              <v-btn size="xs" text class="flex gap-3 items-center" v-on="on">
+            <template #activator>
+              <VDropdownButton :as="VBtn" size="xs" text class="flex gap-3 items-center">
                 <div>
                   <VAvatar
                     src="https://cdn.vuetifyjs.com/images/lists/1.jpg"
@@ -63,7 +65,7 @@ const toggleMenu = () => {
                   </p>
                 </div>
                 <v-icon name="ion:chevron-down-outline" class="w-4 h-4 text-gray-500" />
-              </v-btn>
+              </VDropdownButton>
             </template>
           </v-dropdown>
         </div>
