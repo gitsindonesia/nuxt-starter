b0cb582dacb64eb6a87000fa4d56f4cc22c2f7c3
Warsono <warsono16694@gmail.com>
2022-11-08 14:33:25 +0700

    feat: new layout `HeaderOnly`

---
+++ components/layouts/HeaderOnly.vue
@@ -0,0 +1,13 @@
+<template>
+  <VAppBar shadow="xl" class="py-3">
+    <div class="container flex items-center justify-between mx-auto px-4">
+      <slot name="logo">
+        <v-logo img-class="h-8" />
+      </slot>
+      <slot name="right" />
+    </div>
+  </VAppBar>
+  <div class="container mx-auto p-4 sm:mt-3">
+    <slot />
+  </div>
+</template>
---
+++ pages/help.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'blank',
+})
+
+useHead({
+  title: 'Help',
+})
+
+const { user } = useAuthStore()
+</script>
+
+<template>
+  <LayoutsHeaderOnly>
+    <template #right>
+      <VBtn v-if="!user" to="/auth/login" color="primary">
+        Masuk
+      </VBtn>
+      <VBtn v-else to="/dashboard" color="primary">
+        Back to Dashboard
+        <VIcon name="mdi-arrow-right" class="ml-2" />
+      </VBtn>
+    </template>
+
+    <div class="p-6 rounded-lg bg-white">
+      <header class="text-2xl font-bold mb-4">
+        <h1>Help</h1>
+      </header>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed aut, earum vitae obcaecati quis nostrum excepturi modi minus! Magni veritatis eius dignissimos architecto repellendus delectus totam saepe eum, alias quibusdam.
+      </p>
+    </div>
+  </LayoutsHeaderOnly>
+</template>
