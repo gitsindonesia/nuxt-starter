c6f6aa05f0871bc1513449ae99c0bf282a397dfb
Warsono <warsono16694@gmail.com>
2022-11-10 13:11:43 +0700

    fix: responsive dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -21,19 +21,23 @@ const stats = ref([
     icon: 'ri:price-tag-3-fill',
   },
 ])
+
+const isMobile = useMediaQuery('(max-width: 768px)')
 </script>
 
 <template>
   <div>
-    <div class="grid grid-cols-2 gap-6 mb-6">
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
       <VCard
         v-for="stat in stats"
         :key="stat.title"
         shadow="none"
-        class="card-stats px-3"
+        class="card-stats p-2 sm:px-3"
         :class="`card-stats-${stat.color}`"
+        hide-header
+        hide-footer
       >
-        <div class="flex items-center gap-6">
+        <div class="flex items-center gap-2 sm:gap-6">
           <div class="bg-white rounded-full w-[48px] h-[48px] grid place-items-center">
             <VIcon :name="stat.icon" class="w-[30px] h-30px" />
           </div>
@@ -41,7 +45,7 @@ const stats = ref([
             <div class="text-primary-black font-semibold text-xs">
               {{ stat.title }}
             </div>
-            <div class="text-2xl font-bold text-[var(--card-text-color)]">
+            <div class="text-lg sm:text-2xl font-bold text-[var(--card-text-color)]">
               {{ stat.value }}
             </div>
           </div>
@@ -53,12 +57,12 @@ const stats = ref([
       <h3 class="font-bold text-lg">
         Riwayat Penjualan <span class="text-gray-500">(Hari ini)</span>
       </h3>
-      <div class="flex justify-between items-center mt-6">
+      <div class="flex flex-col sm:flex-row justify-between items-center mt-6 gap-2">
         <div class="space-x-2">
-          <VBtn outlined color="primary">
+          <VBtn outlined :block="isMobile" color="primary">
             Selesai
           </VBtn>
-          <VBtn outlined color="primary">
+          <VBtn outlined :block="isMobile" color="primary">
             Batal
           </VBtn>
         </div>
