02349d9552314d4bad0b4b9ba35fb6d6b10176f3
Warsono <warsono16694@gmail.com>
2022-11-14 11:09:54 +0700

    feat: linkable app-logo

--- components/app/Logo.vue
+++ components/app/Logo.vue
@@ -1,3 +1,26 @@
+<script lang="ts">
+export default defineComponent({
+  inheritAttrs: false,
+})
+</script>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import type { RouteLocation } from 'vue-router'
+
+type LinkProps = Omit<InstanceType<typeof RouterLink>['$props'], 'to'>
+
+withDefaults(defineProps<{
+  to?: RouteLocation | string
+  linkAttrs?: LinkProps
+}>(), {
+  linkAttrs: () => ({} as LinkProps),
+  to: '/',
+})
+</script>
+
 <template>
-  <img src="/logo.webp" alt="Logo">
+  <RouterLink :to="to" v-bind="linkAttrs">
+    <img src="/logo.webp" alt="Logo" v-bind="$attrs">
+  </RouterLink>
 </template>
