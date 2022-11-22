2e071dcbcfe354e31fe26bfd940149c685fcd29a
Warsono <warsono16694@gmail.com>
2022-11-22 21:47:39 +0700

    fix(Layout Two Column): change responsive breakpoint

--- components/layouts/TwoColumn.vue
+++ components/layouts/TwoColumn.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="sm:h-screen block sm:flex">
-    <div class="sm:w-6/12 bg-primary text-white flex items-center justify-center">
+  <div class="md:h-screen block md:flex">
+    <div class="md:w-6/12 bg-primary text-white flex items-center justify-center">
       <slot name="left">
         &nbsp;
       </slot>
     </div>
-    <div class="sm:w-6/12 flex items-center justify-center bg-white">
+    <div class="md:w-6/12 flex items-center justify-center bg-white">
       <slot />
     </div>
   </div>
