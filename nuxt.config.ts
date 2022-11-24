3c8cf83e8fc2da36ca9cb8d60bb7351583b1fb93
Warsono <warsono16694@gmail.com>
2022-11-24 23:04:55 +0700

    feat: setup Poppins font via nuxt google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: [
+    '@nuxtjs/google-fonts',
     '@nuxtjs/tailwindcss',
     ['@pinia/nuxt', { disableVuex: true }],
     '@vueuse/nuxt',
@@ -23,6 +24,14 @@ export default defineNuxtConfig({
           ws: true,
         },
       },
+    },
+  },
+  googleFonts: {
+    prefetch: true,
+    preconnect: true,
+    useStylesheet: true,
+    families: {
+      Poppins: true,
     },
   },
 })
