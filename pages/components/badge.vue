9319e529fd6f829152c83cdee3bdba75ee41c869
Warsono <warsono16694@gmail.com>
2022-11-28 16:09:33 +0700

    feat(badge): add more sample

--- pages/components/badge.vue
+++ pages/components/badge.vue
@@ -17,6 +17,56 @@ import { colors } from './constants'
         </VBadge>
       </div>
     </AppCard>
+
+    <AppCard>
+      <AppCardTitle>
+        Rounded
+      </AppCardTitle>
+
+      <div class="space-x-2">
+        <VBadge v-for="color in colors" :key="color" :color="color" rounded>
+          {{ color }}
+        </VBadge>
+      </div>
+    </AppCard>
+
+    <AppCard>
+      <AppCardTitle>
+        Sizes
+      </AppCardTitle>
+
+      <div class="space-x-2">
+        <VBadge small>
+          small
+        </VBadge>
+        <VBadge>
+          default
+        </VBadge>
+        <VBadge large>
+          large
+        </VBadge>
+      </div>
+    </AppCard>
+
+    <AppCard>
+      <AppCardTitle>
+        Bubble
+      </AppCardTitle>
+
+      <div class="space-x-2">
+        <VBtn
+          prefix-icon="ri:notification-4-line"
+          fab
+          icon
+          text
+          class="relative"
+        >
+          <VBadge color="error" class="absolute rounded-full -top-1 -right-2" small>
+            20
+          </VBadge>
+        </VBtn>
+      </div>
+    </AppCard>
   </div>
 </template>
 
