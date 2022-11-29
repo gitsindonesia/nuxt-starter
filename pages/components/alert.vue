e9de6886929f96a7479edd7e5f84fcee04e9ebc3
Warsono <warsono16694@gmail.com>
2022-11-29 11:29:16 +0700

    feat(alert): more sample

--- pages/components/alert.vue
+++ pages/components/alert.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-//
+const isOpen = ref(true)
+const alert2 = ref(true)
 </script>
 
 <template>
@@ -72,12 +73,121 @@
 
     <AppCard>
       <AppCardTitle>
+        Outlined
+      </AppCardTitle>
+
+      <div class="space-y-2">
+        <VAlert outlined>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert outlined color="primary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert outlined color="secondary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert outlined color="info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert outlined color="warning">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert outlined color="success">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert outlined color="error">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert outlined color="dark">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+      </div>
+    </AppCard>
+
+    <AppCard>
+      <AppCardTitle>
+        Bordered
+      </AppCardTitle>
+
+      <div class="space-y-2">
+        <VAlert bordered>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert bordered color="primary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert bordered color="secondary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert bordered color="info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert bordered color="warning">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert bordered color="success">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert bordered color="error">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+        <VAlert bordered color="dark">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+      </div>
+    </AppCard>
+
+    <AppCard>
+      <AppCardTitle>
+        v-model
+      </AppCardTitle>
+
+      <div class="space-y-2">
+        <VBtn @click="isOpen = !isOpen">
+          Toggle Alert
+        </VBtn>
+        <VAlert
+          v-model="isOpen"
+          color="primary"
+          class="mt-4"
+          solid
+          dismissable
+        >
+          Alert text
+        </VAlert>
+      </div>
+    </AppCard>
+
+    <AppCard>
+      <AppCardTitle>
         Dismissable
       </AppCardTitle>
 
       <div class="space-y-2">
         <VAlert color="primary" dismissable>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </VAlert>
+      </div>
+    </AppCard>
+
+    <AppCard>
+      <AppCardTitle>
+        Custom Transition
+      </AppCardTitle>
+
+      <div class="space-y-2">
+        <VBtn @click="alert2 = !alert2">
+          Toggle Alert
+        </VBtn>
+        <VAlert
+          v-model="alert2"
+          color="primary"
+          class="mt-4"
+          solid
+          dismissable
+          transition="slide-down"
+        >
+          Alert text
         </VAlert>
       </div>
     </AppCard>
