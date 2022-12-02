a43c2fb89549edc8a3f7978725f0c3a2098338c0
Warsono <warsono16694@gmail.com>
2022-12-02 18:10:26 +0700

    feat(tabs): add tabs example

--- components/app/Sidebar.vue
+++ components/app/Sidebar.vue
@@ -69,6 +69,10 @@ const menus = ref([
         text: 'Pagination',
         to: '/components/pagination',
       },
+      {
+        text: 'Tabs',
+        to: '/components/tabs',
+      },
     ],
   },
   {
