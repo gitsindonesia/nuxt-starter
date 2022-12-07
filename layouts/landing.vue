e628a94e69fea876eb8418187140709be0d60965
Warsono <warsono16694@gmail.com>
2022-12-07 22:13:09 +0700

    feat: update landing page

---
+++ layouts/landing.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+const menus = ref([
+  {
+    text: 'Home',
+    to: '/',
+  },
+  {
+    text: 'Features',
+    to: '#features',
+  },
+  {
+    text: 'Review',
+    to: '#review',
+  },
+  {
+    text: 'Pricing',
+    to: '#pricing',
+  },
+  {
+    text: 'FAQs',
+    to: '#faq',
+  },
+])
+</script>
+
+<template>
+  <div class="bg-white">
+    <VAppBar
+      :style="{
+        '--app-bar-bg-color': 'transparent',
+        '--app-bar-height': 'auto',
+      }"
+    >
+      <!-- desktop -->
+      <div class="hidden container mx-auto sm:px-4 sm:flex gap-8 items-center py-3">
+        <AppLogo class="h-8" />
+
+        <nav class="flex-1">
+          <ul class="flex gap-4">
+            <li v-for="menu in menus" :key="menu.text">
+              <NuxtLink :to="menu.to" class="text-sm px-3 py-2 rounded-full text-slate-700 hover:text-slate-900">
+                {{ menu.text }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div class="flex gap-4">
+          <VBtn rounded text to="/auth/login">
+            Sign in
+          </VBtn>
+          <VBtn color="primary" rounded to="/auth/register">
+            Get started today
+          </VBtn>
+        </div>
+      </div>
+
+      <!-- mobile -->
+      <div class="flex sm:hidden items-center w-full">
+        <AppLogo class="h-8" />
+        <div class="flex items-center gap-2 justify-end flex-1">
+          <VBtn color="primary" rounded>
+            Get started today
+          </VBtn>
+          <VMenus :items="menus" right>
+            <VBtn icon rounded fab>
+              <VIcon name="ic:menu" />
+            </VBtn>
+          </VMenus>
+        </div>
+      </div>
+    </VAppBar>
+
+    <slot />
+
+    <footer class="py-4">
+      <div class="container mx-auto flex flex-col sm:flex-row gap-4 justify-between items-center">
+        <AppLogo class="h-6" />
+        <div class="text-xs text-zinc-600">
+          Copyright &copy; 2022. PT GITS Indonesia. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,182 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'blank',
+  layout: 'landing',
 })
 
 useHead({
   title: 'Home',
 })
+
+const features = ref([
+  {
+    name: 'Vue 3',
+    description: 'The latest and greatest Vue.js version.',
+    icon: 'logos:vue',
+  },
+  {
+    name: 'Tailwind CSS',
+    description: 'Utility-first CSS framework.',
+    icon: 'logos:tailwindcss-icon',
+  },
+  {
+    name: 'Nuxt 3',
+    description: 'The latest and greatest Nuxt.js version.',
+    icon: 'logos:nuxt-icon',
+  },
+  {
+    name: 'CSS Properties',
+    description: 'The latest and greatest CSS properties.',
+    icon: 'logos:css-3',
+  },
+])
+
+const features2 = ref([
+  {
+    name: 'Simple',
+    description: 'Simplify your workflow with a simple and clean UI.',
+    icon: 'ri:layout-2-fill',
+  },
+  {
+    name: 'Dark Mode',
+    description: 'Enjoy the dark mode in your application.',
+    icon: 'ri:moon-fill',
+  },
+  {
+    name: 'Fast',
+    description: 'Fast and responsive application.',
+    icon: 'ri:speed-fill',
+  },
+  {
+    name: 'Secured',
+    description: 'Secured application with authentication and authorization.',
+    icon: 'ri:lock-fill',
+  },
+  {
+    name: 'Mobile-first',
+    description: 'Mobile-first application with responsive design.',
+    icon: 'ri:smartphone-fill',
+  },
+  {
+    name: 'Customizable',
+    description: 'Customizable application with Tailwind CSS and CSS Properties.',
+    icon: 'ri:paint-brush-fill',
+  },
+])
 </script>
 
 <template>
-  <LayoutsHeaderOnly>
-    <div class="container mx-auto grid place-items-center sm:h-screen">
-      <Counter />
-    </div>
-  </LayoutsHeaderOnly>
+  <div>
+    <section class="container mx-auto flex py-20 sm:py-10 sm:h-[calc(100vh-100px)] items-center">
+      <div class="mx-auto space-y-5 sm:space-y-7 px-4 sm:px-0">
+        <h1 class="text-2xl sm:text-5xl tracking-tight font-semibold text-center leading-tight text-slate-900">
+          Application <span class="text-primary">made simple</span><br>
+          for your business
+        </h1>
+        <p class="text-slate-700 text-center max-w-2xl text-base sm:text-lg">
+          All features and tools that you need to build your application with
+          comfort and fun.
+        </p>
+        <div class="flex gap-4 justify-center items-center">
+          <VBtn color="dark" rounded>
+            Get 6 months free
+          </VBtn>
+          <VBtn prefix-icon="ri:play-fill" rounded outlined>
+            Watch video
+          </VBtn>
+        </div>
+      </div>
+    </section>
+
+    <!-- trusted section -->
+    <section class="py-10 sm:py-24">
+      <div class="container mx-auto">
+        <p class="text-center text-slate-700">
+          Trusted by these six companies so far
+        </p>
+        <div class="px-6 sm:px-0 mt-7 max-w-2xl mx-auto grid gap-4 sm:gap-6 grid-cols-3 grid-cols-6 items-center justify-center">
+          <AppLogo v-for="i in 6" :key="i" class="h-6 sm:h-8" />
+        </div>
+      </div>
+    </section>
+
+    <!-- feature section -->
+    <section class="py-10 sm:py-24 bg-zinc-900 px-6 sm:px-0">
+      <div class="container mx-auto">
+        <div class="max-w-4xl space-y-5">
+          <h2 class="text-2xl sm:text-4xl font-semibold text-white">
+            Every feature you need to win. Try it for yourself.
+          </h2>
+          <p class="text-zinc-500 text-base sm:text-lg">
+            Pocket was built for investors like you who play by their own rules and aren’t going to let SEC regulations get in the way of their dreams. If other investing tools are afraid to build it, Pocket has it.
+          </p>
+        </div>
+        <div class="mt-10 sm:mt-24 grid grid-cols-1 sm:grid-cols-2 gap-8">
+          <div class="hidden sm:block">
+            <div class="bg-zinc-800 rounded-xl h-[790px]" />
+          </div>
+          <div class="flex flex-col gap-8">
+            <div
+              v-for="feature in features"
+              :key="feature.name"
+              class="space-y-4 rounded-xl p-8 border border-zinc-800 hover:bg-zinc-800 transition duration-300"
+            >
+              <VIcon :name="feature.icon" size="lg" />
+              <h3 class="text-zinc-300 font-semibold">
+                {{ feature.name }}
+              </h3>
+              <p class="text-sm text-zinc-500">
+                {{ feature.description }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- feature 2 section -->
+    <section class="py-10 sm:py-24 px-6 sm:px-0">
+      <div class="container mx-auto">
+        <div class="max-w-4xl mx-auto space-y-5">
+          <h2 class="text-2xl sm:text-4xl font-medium text-left sm:text-center">
+            Now is the time to build your portfolio.
+          </h2>
+          <p class="text-left sm:text-center text-base text-lg text-slate-700">
+            With typical market returns, you have to start young to secure your future. With Pocket, it’s never too late to build your nest egg.
+          </p>
+        </div>
+        <div class="mt-10 sm:mt-24">
+          <div class="grid grid-cols-1 sm:grid-cols-3 gap-8">
+            <div
+              v-for="feature in features2"
+              :key="feature.name"
+              class="space-y-4 rounded-xl p-8 border border-slate-200"
+            >
+              <VIcon :name="feature.icon" size="xl" class="shadow-sm p-2 rounded-lg" />
+              <h3 class="text-zinc-900 font-medium">
+                {{ feature.name }}
+              </h3>
+              <p class="text-sm text-zinc-700">
+                {{ feature.description }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- CTA -->
+    <section class="py-10 sm:py-24 bg-zinc-900 px-6 sm:px-0">
+      <div class="sm:max-w-4xl mx-auto text-center space-y-5">
+        <h3 class="text-white text-3xl sm:text-4xl font-medium">
+          Get your first tips today
+        </h3>
+        <p class="text-zinc-200 text-base sm:max-w-md leading-relaxed mx-auto">
+          It takes 30 seconds to sign up. Download the app and create an account today and we’ll send you a tip guaranteed to double your first investment.
+        </p>
+        <VBtn rounded>
+          Get started
+        </VBtn>
+      </div>
+    </section>
+  </div>
 </template>
