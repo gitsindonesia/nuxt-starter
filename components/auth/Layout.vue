914c1fb5e3925e08e472b27117e85e9963d7edea
Warsono <warsono16694@gmail.com>
2022-12-08 11:42:13 +0700

    chore: rename folders

---
+++ components/auth/Layout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="grid place-items-center sm:h-screen">
+    <div class="w-full sm:max-w-xl">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ components/modal/ModalDelete.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import type { VModalEvent } from '@gits-id/modal'
+import { useVModel } from '@vueuse/core'
+
+interface Props {
+  modelValue?: boolean
+  title?: string
+  message?: string
+  loading?: boolean
+  api?: any
+  successTitle?: string
+  successMessage?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: false,
+  title: 'Remove Item',
+  message: 'Are you sure want to remove this item?',
+  loading: false,
+  successTitle: 'Success',
+  successMessage: 'Item removed successfully!',
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+  (e: 'update:loading', value: boolean): void
+  (e: 'confirm'): void
+}>()
+
+const { loading } = toRefs(props)
+
+const isOpen = useVModel(props, 'modelValue', emit)
+const isLoading = ref(props.loading)
+const isSuccess = ref(false)
+
+watch(
+  loading,
+  (val) => {
+    isLoading.value = val
+  },
+  { immediate: true },
+)
+
+const onConfirm = (e: VModalEvent) => {
+  isLoading.value = true
+  const { error } = props.api()
+  isLoading.value = false
+
+  if (!error.value) {
+    e.close()
+    setTimeout(() => {
+      isSuccess.value = true
+    }, 500)
+  }
+}
+</script>
+
+<template>
+  <v-modal
+    v-model="isOpen"
+    :title="title"
+    :close-props="{ block: true }"
+    confirm
+    :confirm-props="{ color: 'error', block: true }"
+    :loading="isLoading"
+    body-class="text-center"
+    hide-header
+    footer-class="flex-row-reverse"
+    @confirm="onConfirm"
+  >
+    <div
+      class="bg-warning-100 h-14 w-14 rounded-full grid place-items-center mx-auto"
+    >
+      <v-icon
+        name="heroicons-outline:question-mark-circle"
+        class="w-10 h-10 text-warning-500"
+      />
+    </div>
+    <div class="mt-4">
+      <h3 class="font-semibold text-lg mb-2">
+        {{ title }}
+      </h3>
+      <p>{{ message }}</p>
+    </div>
+  </v-modal>
+
+  <ModalSuccess
+    v-model="isSuccess"
+    :title="successTitle"
+    :message="successMessage"
+    @close="isSuccess = false"
+  />
+</template>
