e72aaaf9b421519ce2e6894c60bfae990753aed8
Warsono <warsono16694@gmail.com>
2022-12-08 14:35:55 +0700

    chore: responsive layout

--- components/layouts/TwoColumn.vue
+++ components/layouts/TwoColumn.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="md:h-screen block md:flex">
-    <div class="md:w-6/12 bg-gradient-to-r from-cyan-500 to-primary-500 text-white flex items-center justify-center">
+    <div
+      class="md:w-6/12 bg-gradient-to-r from-cyan-500 to-primary-500 text-white flex items-center justify-center
+      p-2
+    "
+    >
       <slot name="left">
         <img
           src="/login.svg"
           alt="illustration"
           width="460"
           heigt="460"
-          class="sm:w-[460px] sm:h-[460px]"
+          class="hidden sm:block sm:w-[460px] sm:h-[460px]"
         >
       </slot>
     </div>
