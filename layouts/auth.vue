e05088f87467c2aff3c81be582e7594fa69dd8f2
Warsono <warsono16694@gmail.com>
2022-12-08 14:47:03 +0700

    refactor: convert two-column component as layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,3 +1,22 @@
 <template>
-  <slot />
+  <div class="md:h-screen block md:flex">
+    <div
+      class="md:w-6/12 bg-gradient-to-r from-cyan-500 to-primary-500 text-white flex items-center justify-center
+      p-2 sm:p-8
+    "
+    >
+      <slot name="left">
+        <img
+          src="/login.svg"
+          alt="illustration"
+          width="460"
+          heigt="460"
+          class="hidden sm:block sm:w-[460px] sm:h-[460px]"
+        >
+      </slot>
+    </div>
+    <div class="md:w-6/12 flex items-center justify-center bg-white">
+      <slot />
+    </div>
+  </div>
 </template>
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'blank',
+  layout: 'auth',
   middleware: 'guest',
 })
 
@@ -12,58 +12,56 @@ const { handleSubmit, showAlert, showPassword, error, loading } = useLogin()
 </script>
 
 <template>
-  <LayoutsTwoColumn>
-    <div class="rounded-lg p-6 sm:min-w-[420px]">
-      <AppLogo class="h-10" />
+  <div class="rounded-lg p-6 sm:min-w-[420px]">
+    <AppLogo class="h-10" />
 
-      <h1 class="text-2xl font-bold mt-6 text-gray-800">
-        Masuk Akun
-      </h1>
-      <p class="text-gray-500 mt-3">
-        Silahkan login untuk mengakses semua fitur!
-      </p>
+    <h1 class="text-2xl font-bold mt-6 text-gray-800">
+      Masuk Akun
+    </h1>
+    <p class="text-gray-500 mt-3">
+      Silahkan login untuk mengakses semua fitur!
+    </p>
 
-      <v-alert v-model="showAlert" color="error" class="mt-6">
-        {{ error }}
-      </v-alert>
+    <v-alert v-model="showAlert" color="error" class="mt-6">
+      {{ error }}
+    </v-alert>
 
-      <form class="mt-5" @submit="handleSubmit">
-        <v-input
-          label="Email"
-          placeholder="Email"
-          name="email"
-          prepend-icon="ri:mail-line"
-        />
-        <v-input
-          wrapper-class="mt-4"
-          label="Password"
-          placeholder="Password"
-          name="password"
-          :type="showPassword ? 'text' : 'password'"
-          :append-icon="showPassword ? 'ri:eye-line' : 'ri:eye-off-line'"
-          prepend-icon="ri:lock-line"
-          @click-append-icon="showPassword = !showPassword"
-        />
-        <v-checkbox
-          wrapper-class="mt-5"
-          name="rememberMe"
-          label="Remember me"
-        />
-        <v-btn
-          :loading="loading"
-          type="submit"
-          class="mt-5"
-          color="primary"
-          block
-        >
-          Login
-        </v-btn>
-      </form>
-      <div class="text-center mt-2">
-        <v-btn color="primary" text to="/auth/forgot-password">
-          Forgot Password?
-        </v-btn>
-      </div>
+    <form class="mt-5" @submit="handleSubmit">
+      <v-input
+        label="Email"
+        placeholder="Email"
+        name="email"
+        prepend-icon="ri:mail-line"
+      />
+      <v-input
+        wrapper-class="mt-4"
+        label="Password"
+        placeholder="Password"
+        name="password"
+        :type="showPassword ? 'text' : 'password'"
+        :append-icon="showPassword ? 'ri:eye-line' : 'ri:eye-off-line'"
+        prepend-icon="ri:lock-line"
+        @click-append-icon="showPassword = !showPassword"
+      />
+      <v-checkbox
+        wrapper-class="mt-5"
+        name="rememberMe"
+        label="Remember me"
+      />
+      <v-btn
+        :loading="loading"
+        type="submit"
+        class="mt-5"
+        color="primary"
+        block
+      >
+        Login
+      </v-btn>
+    </form>
+    <div class="text-center mt-2">
+      <v-btn color="primary" text to="/auth/forgot-password">
+        Forgot Password?
+      </v-btn>
     </div>
-  </LayoutsTwoColumn>
+  </div>
 </template>
