ff7376206e550849547a86beb9669ddd215212bd
Warsono <warsono16694@gmail.com>
2022-12-09 08:31:06 +0700

    feat: update product card

---
+++ composables/helpers.ts
@@ -0,0 +1,6 @@
+export const toCurrency = (value: number) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(value)
+}
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -26,19 +26,37 @@ const { data, error, pending } = await useLazyAsyncData(() => getProducts())
       <NuxtLink
         v-for="product in data.products"
         :key="product.id"
-        class="shadow rounded-lg overflow-hidden bg-white pb-4 transition duration-300 hover:shadow-lg"
+        class="shadow-md shadow-gray-300/20 group relative rounded-lg overflow-hidden bg-white pb-4 transition duration-300 hover:shadow-lg hover:scale-105"
         :to="`/products/${product.id}`"
       >
-        <img
-          :src="product.thumbnail"
-          :alt="product.title"
-          width="100%"
-          height="100"
-          class="w-full aspect-1 rounded-lg  object-cover bg-gray-100"
-        >
-        <h3 class="font-medium text-lg mt-4 px-4">
-          {{ product.title }}
-        </h3>
+        <div class="relative">
+          <img
+            :src="product.thumbnail"
+            :alt="product.title"
+            width="100%"
+            height="100"
+            class="w-full aspect-1 rounded-t-lg  object-cover bg-gray-100"
+          >
+          <VBtn
+            fab
+            text
+            prefix-icon="ri:heart-line"
+            class="absolute top-2 right-2 hidden group-hover:inline"
+          />
+          <VBtn
+            color="secondary"
+            fab
+            icon
+            prefix-icon="ri:shopping-basket-line"
+            class="absolute bottom-2 right-2 hidden group-hover:inline"
+          />
+        </div>
+        <div class="px-4 space-y-2 mt-4">
+          <h3 class="font-medium">
+            {{ product.title }}
+          </h3>
+          <div> {{ toCurrency(product.price) }} </div>
+        </div>
       </NuxtLink>
     </div>
   </div>
