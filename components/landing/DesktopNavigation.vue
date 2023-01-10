1c2852530dd88a7fdbe764745f64763885758157
Warsono <warsono16694@gmail.com>
2023-01-10 11:57:16 +0700

    feat: centered menu

--- components/landing/DesktopNavigation.vue
+++ components/landing/DesktopNavigation.vue
@@ -7,14 +7,17 @@ defineProps<{
 </script>
 
 <template>
-  <div class="hidden lg:container mx-auto px-4 sm:px-0 lg:flex gap-4 xl:gap-8 items-center py-1">
+  <div
+    class="hidden lg:container mx-auto px-4 sm:px-0 lg:flex gap-4 xl:gap-8
+      items-center py-1 justify-between"
+  >
     <Logo
       width="80"
       height="36"
     />
 
-    <nav class="flex-1">
-      <ul class="flex gap-4">
+    <nav aria-label="Desktop Navigation">
+      <ul class="flex justify-center gap-4">
         <LandingDesktopNavItem
           v-for="menu in menus"
           :key="menu.text"
