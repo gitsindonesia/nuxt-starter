174dd5edf2d91d2625c125fa4ccae2ffb91a6b74
Warsono <warsono16694@gmail.com>
2023-01-11 22:03:08 +0700

    fix: improve a11y

--- components/landing/DesktopNavItem.vue
+++ components/landing/DesktopNavItem.vue
@@ -14,7 +14,7 @@ defineProps<{
       class="text-base px-3 py-2 rounded-full dark:text-gray-400 dark:hover:text-gray-100 text-gray-600 hover:text-gray-900 flex items-center gap-2"
       exact
       :exact-active-class="menu.isActive ? '' : '!text-primary-600 font-semibold'"
-      :class="menu.isActive?.() ? '!text-primary font-semibold' : ''"
+      :class="menu.isActive?.() ? '!text-primary-600 font-semibold' : ''"
       :target="menu.newTab ? '_blank' : undefined"
       :rel="menu.newTab ? 'noreferef' : undefined"
     >
