2e6f4fefbe41ca89a679c760d52e938219d3219c
Warsono <warsono16694@gmail.com>
2023-01-11 20:45:23 +0700

    chore: remove unused files

--- plugins/quill.client.ts
+++
@@ -1,6 +0,0 @@
-import { VQuillEditor } from '@gits-id/quill-editor'
-import '@gits-id/quill-editor/dist/style.css'
-
-export default defineNuxtPlugin((nuxtapp) => {
-  nuxtapp.vueApp.component('VQuillEditor', VQuillEditor)
-})
