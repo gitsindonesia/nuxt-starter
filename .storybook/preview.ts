df6266cf7d63696a0adc4a880ebaca2f3eb721ee
Warsono <warsono16694@gmail.com>
2023-10-06 10:12:52 +0700

    chore: update storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,16 @@
+import {Preview} from '@storybook/vue3'
+import './tailwind.css'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
